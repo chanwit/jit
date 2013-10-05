5872772371dbeb5406d9e80becfa697bf25ab925
Chanwit Kaewkasi <chanwit@gmail.com>
2013-10-06 00:56:42 +0700

    implemented label and branching

--- jit.go
+++ jit.go
@@ -33,9 +33,18 @@ type Value struct {
     C C.jit_value_t
 }
 
+type Label struct {
+    C *C.jit_label_t
+}
+
 var intType = Type{ C.jit_type_int }
 func Int() Type {
     return intType
+}
+
+var uintType = Type{ C.jit_type_uint }
+func UInt() Type {
+    return uintType
 }
 
 func NewContext() *Context {
@@ -44,7 +53,7 @@ func NewContext() *Context {
 
 func NewSignature(ret Type, params []Type) *Signature {
     signature := C.jit_type_create_signature(C.jit_abi_cdecl,
-        C.jit_type_int,
+        ret.C,
         (*C.jit_type_t)(unsafe.Pointer(&params[0])),
         C.uint(len(params)), 1)
     return &Signature{signature}
@@ -92,8 +101,38 @@ func (f *Function) Add(a, b *Value) *Value {
     return &Value{ C.jit_insn_add(f.C, a.C, b.C) }
 }
 
+func (f *Function) Sub(a, b *Value) *Value {
+    return &Value{ C.jit_insn_sub(f.C, a.C, b.C) }
+}
+
 func (f *Function) Return(ret *Value) {
     C.jit_insn_return(f.C, ret.C)
+}
+
+func (f *Function) Eq(a, b *Value) *Value {
+    return &Value{ C.jit_insn_eq(f.C, a.C, b.C) }
+}
+
+func (f *Function) BranchIfNot(v *Value, label *Label) {
+    C.jit_insn_branch_if_not(f.C, v.C, label.C)
+}
+
+func (f *Function) Label(label *Label) {
+    C.jit_insn_label(f.C, label.C)
+}
+
+func (f *Function) LessThan(a, b *Value) *Value {
+    return &Value{ C.jit_insn_lt(f.C, a.C, b.C) }
+}
+
+func (f *Function) Call(target *Function, values []*Value) *Value {
+    args := make([]C.jit_value_t, len(values))
+    for i := range values {
+        args[i] = values[i].C
+    }
+    return &Value{ C.jit_insn_call(f.C,
+                    C.CString("noname"),
+                    target.C, nil, (*C.jit_value_t)(&args[0]), C.uint(len(args)), C.int(0)) }
 }
 
 func (f *Function) Compile() {
@@ -107,6 +146,10 @@ func (f *Function) Run(values ...interface{}) interface{} {
             case C.jit_type_int:
                 x := C.jit_int(values[i].(int))
                 args[i] = (unsafe.Pointer)(&x)
+
+            case C.jit_type_uint:
+                x := C.jit_uint(values[i].(uint))
+                args[i] = (unsafe.Pointer)(&x)
         }
     }
 
@@ -116,11 +159,22 @@ func (f *Function) Run(values ...interface{}) interface{} {
             C.jit_function_apply(f.C, (*unsafe.Pointer)(&args[0]), unsafe.Pointer(&result))
             return int(result)
 
+        case C.jit_type_uint:
+            result := C.jit_uint(0)
+            C.jit_function_apply(f.C, (*unsafe.Pointer)(&args[0]), unsafe.Pointer(&result))
+            return uint(result)
     }
 
     return nil
 }
 
-func (f *Function) Dump() {
-    C.jit_dump_function((*C.FILE)(C.stdout), f.C, C.CString("main"))
+func (f *Function) Dump(name string) {
+    C.jit_dump_function((*C.FILE)(C.stdout), f.C, C.CString(name))
+}
+
+func NewLabel() *Label {
+    result := &Label{}
+    result.C = new(C.jit_label_t)
+    (*result.C) = C.jit_label_undefined
+    return result
 }
