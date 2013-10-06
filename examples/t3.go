b6a486ca53d46538b320242fd14464d26cbe38af
Chanwit Kaewkasi <chanwit@gmail.com>
2013-10-06 16:23:46 +0700

    add t3 the on-demand example

---
+++ examples/t3.go
@@ -0,0 +1,46 @@
+package main
+/*
+#cgo LDFLAGS: -lm -ljit
+#include <jit/jit.h>
+#include <jit/jit-dump.h>
+*/
+import "C"
+import "fmt"
+import . "../../jit"
+
+func compile_mul_add(function *Function) bool {
+    fmt.Println(">> Recompile ...")
+    x, y, z := function.Param3()
+    t1 := function.Mul(x, y)
+    t2 := function.Add(t1, z)
+    function.Return(t2)
+
+    // compile successfully
+    return true
+}
+
+func main() {
+    ctx := NewContext()
+    defer ctx.Destroy()
+
+    ctx.BuildStart()
+
+    f := ctx.NewFunction(Int(), []Type{Int(), Int(), Int()})
+    f.SetRecompilable()
+    f.SetOnDemandCompiler(compile_mul_add)
+
+    ctx.BuildEnd()
+
+    // First-time on-demand compiler will be called
+    fmt.Printf("mul_add(3, 5, 2) = %d\n", f.Run(3, 5, 2).(int))
+
+    // Second-time, nothing compiled
+    fmt.Printf("mul_add(13, 5, 7) = %d\n", f.Run(13, 5, 7).(int))
+
+    ctx.BuildStart()
+    f.GetOnDemandCompiler()(f) // return closure
+    f.Compile()
+    ctx.BuildEnd()
+
+    fmt.Printf("mul_add(2, 18, -3) = %d\n", f.Run(2, 18, -3).(int))
+}
